55f9c95a5c9edbfcee06ac856b1f7a5e028c3870
HoaiNam-1632 <nhnam1632@gmail.com>
2021-11-01 14:39:49 +0700

    update register

--- src/Http/Controllers/AuthController.php
+++ src/Http/Controllers/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
         try {
             DB::beginTransaction();
             $req = $request->all();
-
+            $req['rules'] = ["user"];
             $req['password'] = Hash::make($req['password']);
             $user = User::create($req);
             DB::commit();
